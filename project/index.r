09ddad3609b99d87a3d0c353e9575498c90fed94
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2023-04-04 22:10:09 -0300

    Create index.r

---
+++ project/index.r
@@ -0,0 +1,32 @@
+
+
+library(dplyr)
+library(data.table)
+library(ff)
+system("java -version")
+install.packages("sparklyr")
+packageVersion("sparklyr")
+library(sparklyr)
+spark_install("3.3")
+
+sc <- spark_connect(master = "local", version = "3.3")
+
+#Filtra do arquivo
+sinasc <- Leitos_2020%>%
+  slice_head(n = 50000)
+
+# copia para variável
+dados <- copy_to(sc, sinasc)
+
+#mostra no console
+dados
+
+#Gera o plot das colunas peso e idademãe, com amostra de 100
+select(cars, LEITOS_EXISTENTE, UF) %>%
+  sample_n(100) %>%
+  collect() %>%
+  plot()
+
+#disconecta ? precisa disso ?
+spark_disconnect_all()
+
