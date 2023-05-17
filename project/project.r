cd93d2c2820a9346c09eadecf90f8e91035f9dce
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2023-05-16 22:02:21 -0300

    Update project.r

--- project/project.r
+++ project/project.r
@@ -9,11 +9,11 @@ library(microdatasus)
 
 dados <- fetch_datasus(year_start = 2020, year_end = 2020, uf = "SP", information_system = "SIM-DO")
 
-# Processamento no posix não aguenta este processamento
+# Posix não aguenta este processamento
 # dados <-process_sim(dados)
 
 ##########################
-# PRÉ PROCESSAMENTO
+# Preparação de dados
 ##########################
 
 library(tidyverse)
@@ -24,12 +24,26 @@ library(naivebayes) # executa o algoritmo naive bayes
 library(ggplot2) # Para análise exploratória
 library(pROC) # Para curva ROC
 
+# Junta a base de dados de municípios à base de mortalidade
+dados <- merge(dados, municipios, by.x = 'CODMUNRES', by.y = 'CODMUNIC')
+
 # Ajusta idade
 dados <- transform(dados, IDADE2 = ifelse(as.numeric(as.character(IDADE)) <= 400, 1, as.numeric(as.character(IDADE))))
 dados <- transform(dados, IDADE2 = ifelse(IDADE2 > 1 & IDADE2 < 500, IDADE2 - 400, 100))
 
+
+##########################
+# Modelagem
+##########################
+
 # Verifica outlier
 boxplot(dados$IDADE2)
+
+#Gera o plot das colunas idade e racacor, com amostra de 100
+select(dados, IDADE2, RACACOR) %>%
+  sample_n(100) %>%
+  collect() %>%
+  plot()
 
 #Analise exploratória
 summary(dados)
